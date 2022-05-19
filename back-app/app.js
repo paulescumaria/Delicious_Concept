403cfa48b2caaf1046a89c9b3d1120f5a52c71e2
Paulescu Maria-Alexandra <paulescumariaalexandra@gmail.com>
2022-05-19 20:21:07 +0300

    connect to MongoDB database

--- back-app/app.js
+++ back-app/app.js
@@ -1,6 +1,12 @@
 const express = require('express')
+const mongoose = require('mongoose')
 const app = express()
-const port = 3000
+const port = 8080
+
+mongoose.connect('mongodb://localhost/cookbook', { useNewUrlParser: true})
+const db = mongoose.connection
+db.on('error', (error) => console.error(error))
+db.once('open', () => console.log('Connected to Database'))
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
